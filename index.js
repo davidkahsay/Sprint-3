8f549ae8ae4423b8087998975801de0d69a5b4ee
osmanl1 <124148463+osmanl1@users.noreply.github.com>
2023-03-28 22:16:15 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,79 @@
 /* Import dependencies */
-const express = require("express");
-const mysql = require("mysql2");
+import express from "express";
+import mysql from "mysql2/promise";
 
 /* Create express instance */
 const app = express();
 const port = 3000;
 
+
+app.set("view engine", "pug");
+
+// Serve assets from 'static' folder
+app.use(express.static("static"));
+
+//const db = await DatabaseService.connect();
+//const { conn } = db;
+
+/* Landing route */
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+// Landing route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 /* Setup database connection */
-const db = mysql.createConnection({
+const db = await mysql.createConnection({
   host: process.env.DATABASE_HOST || "localhost",
   user: "user",
   password: "password",
   database: "world",
 });
 
-/* Landing route */
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+
+// Gallery route
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+
+// About route
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// About route
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+// About route
+
+app.get("/contactus", (req, res) => {
+  res.render("contactus");
+});
+
+// Returns an array of cities from the database
+// Returns an array of cities from the database
+app.get("/cities", async (req, res) => {
+  const [rows, fields] = await db.execute("SELECT * FROM `city`");
+  return res.render("cities",{rows,fields});
+  //return res.send(rows);
+});
+
+
+app.get('/cities/:id', async (req, res) => {
+  const cityId = req.params.id;
+  const city = await db.getCity(cityId);
+  return res.render('city', { city });
+})
+
+// Returns JSON array of cities
+app.get("/api/cities", async (req, res) => {
+  const [rows, fields] = await db.getCities();
+  return res.send(rows);
 });
 
 // Sample API route
@@ -25,28 +82,12 @@ app.get("/ping", (req, res) => {
 });
 
 // Returns an array of cities from the database
-app.get("/cities", (req, res) => {
-  db.execute("SELECT * FROM `city`", (err, rows, fields) => {
-    console.log(`/cities: ${rows.length} rows`);
-    return res.send(rows);
-  });
+app.get("/cities", async (req, res) => {
+  const [rows, fields] = await db.execute("SELECT * FROM `city`");
+  return res.send(rows);
 });
 
-// Returns an array of cities from the database
-app.get("/countries", (req, res) => {
-    db.execute("SELECT * FROM `country`", (err, rows, fields) => {
-      console.log(`/cities: ${rows.length} rows`);
-      return res.send(rows);
-    });
-  });
 
-  // Returns an array of cities from the database
-app.get("/countrylanguages", (req, res) => {
-    db.execute("SELECT * FROM `countrylanguage`", (err, rows, fields) => {
-      console.log(`/cities: ${rows.length} rows`);
-      return res.send(rows);
-    });
-  });
 
 // Run server!
 app.listen(port, () => {
