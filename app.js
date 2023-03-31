5acac93b8a357f6ee920af7d6a3c731a8be709fe
kahsayd <kahsayd@roehampton.ac.uk>
2023-03-31 17:15:08 +0100

    Squashed commit of the following:
    commit ed199961d30cdb11f7c9f3c296fa981525168694
    Author: Samira2002 <124591853+Samira2002@users.noreply.github.com>
    Date:   Fri Mar 31 17:14:00 2023 +0100
    
        Update index.js
    
    commit 7e133bbcd7bfb2971d1222f63befd6c03be75538
    Author: osmanl1 <124148463+osmanl1@users.noreply.github.com>
    Date:   Fri Mar 31 16:52:28 2023 +0100
    
        Add files via upload
    
    commit 22732d7e6aba1584f0c71fc2f3d909637c2ec4f1
    Author: Samira2002 <124591853+Samira2002@users.noreply.github.com>
    Date:   Fri Mar 31 16:35:53 2023 +0100
    
        Update cities.css
    
    commit 8bb28da758914ce2c93d3dedcd0207565d2bf1de
    Author: Samira2002 <124591853+Samira2002@users.noreply.github.com>
    Date:   Fri Mar 31 16:32:50 2023 +0100
    
        Update cities.css
    
    commit 8fe2ba095da2896227c453439cb532679e7099ac
    Author: kahsayd <124149416+kahsayd@users.noreply.github.com>
    Date:   Wed Mar 29 11:06:16 2023 +0100
    
        Create 2index.js
    
    commit c685622f5875fb3654b4093da0e9a215198bf8c1
    Author: kahsayd <124149416+kahsayd@users.noreply.github.com>
    Date:   Tue Mar 28 23:54:04 2023 +0100
    
        Update config.yml
    
    commit cd75c73110857947fc8509c35ab882c14089659f
    Author: osmanl1 <124148463+osmanl1@users.noreply.github.com>
    Date:   Tue Mar 28 23:35:32 2023 +0100
    
        Add files via upload
    
    commit 832544c601b4816cf0d20d4a371a56a303557e1d
    Author: osmanl1 <124148463+osmanl1@users.noreply.github.com>
    Date:   Tue Mar 28 23:06:10 2023 +0100
    
        Add files via upload
    
    commit 8f549ae8ae4423b8087998975801de0d69a5b4ee
    Author: osmanl1 <124148463+osmanl1@users.noreply.github.com>
    Date:   Tue Mar 28 22:16:15 2023 +0100
    
        Add files via upload
    
    commit 2625f0fa851c8e2168bf6af337e6ae8b3c6712ca
    Author: osmanl1 <124148463+osmanl1@users.noreply.github.com>
    Date:   Tue Mar 28 22:15:20 2023 +0100
    
        Add files via upload
    
    commit 5d5c865505bf81c6b4f1b388462ca5d857422dbb
    Author: sadia-roehampton <alis50@roehampton.ac.uk>
    Date:   Tue Mar 28 16:34:30 2023 +0100
    
        first node/express
    
    commit d755a2ba758e417dccc7294652c1a13b2a2d1e51
    Author: sumiili <124701146+sumiili@users.noreply.github.com>
    Date:   Tue Mar 28 15:12:10 2023 +0100
    
        Update test.js
    
    commit 15a7e73e3011534c2f3a0dfea5cb5637637ec236
    Author: sumiili <124701146+sumiili@users.noreply.github.com>
    Date:   Tue Mar 28 15:09:06 2023 +0100
    
        Update test.js
    
    commit 039efed9b76e19c77d23dd66b628a6c5814e63de
    Author: sumiili <124701146+sumiili@users.noreply.github.com>
    Date:   Tue Mar 28 14:55:32 2023 +0100
    
        Create test.js
    
    commit b74173a1136fa47ad02cb03e74c7034c2012fa50
    Author: kahsayd <kahsayd@roehampton.ac.uk>
    Date:   Mon Mar 27 14:16:35 2023 +0100
    
        Add existing file

---
+++ app.js
@@ -0,0 +1,47 @@
+// Import express.js
+const express = require("express");
+
+// Create express app
+var app = express();
+
+// Add static files location
+app.use(express.static("static"));
+
+// Get the functions in the db.js file to use
+const db = require('./services/db');
+
+app.get("/country-report", function(req, res) {
+    var sql ='select * from country';
+    var output ='<table border="1px">';
+    db.query(sql).then(results => {
+        for(var row of results) {
+            output += '<tr>';
+            output += '<td>' + row.Code + '</td>';
+            output += '<td>' + row.Name + '</td>';
+            output += '<td>' + row.Continent + '</td>';
+            output += '<td>' + row.Region + '</td>';
+            output += '<td>' + row.Population + '</td>';
+            output += '<td>' + row.Capital + '</td>';
+            output += '</tr>';
+        }
+        output+= '</table>';
+        res.send(output);
+    });
+});
+
+
+// Create a dynamic route for /hello/<name>, where name is any value provided by user
+// At the end of the URL
+// Responds to a 'GET' request
+app.get("/hello/:name", function(req, res) {
+    // req.params contains any parameters in the request
+    // We can examine it in the console for debugging purposes
+    console.log(req.params);
+    //  Retrieve the 'name' parameter and use it in a dynamically generated page
+    res.send("Hello " + req.params.name);
+});
+
+// Start server on port 3000
+app.listen(3000,function(){
+    console.log(`Server running at http://127.0.0.1:3000/`);
+});
